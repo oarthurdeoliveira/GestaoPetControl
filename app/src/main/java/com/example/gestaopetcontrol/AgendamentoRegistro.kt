736d110ce47d6c2c80cf289bb58f987e65fc80ce
Arthur de Oliveira Leite <oarthurdeoliveiraleite@gmail.com>
2026-05-13 19:42:05 -0300

    0.2.1

---
+++ app/src/main/java/com/example/gestaopetcontrol/AgendamentoRegistro.kt
@@ -0,0 +1,32 @@
+package com.example.gestaopetcontrol
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gestaopetcontrol.databinding.ActivityAgendamentoRegistroBinding
+
+class AgendamentoRegistro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAgendamentoRegistroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAgendamentoRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnAgendar.setOnClickListener {
+            intent = Intent(this, Agendamentos::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
